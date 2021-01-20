05fe5a22e6d2b7a5289e989fe89658b714dabb44
Vinicius Almeida <termobin@gmail.com>
2021-01-19 21:32:45 -0300

    Added Express js lib

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const http = require('http');
-const routes = require('./routes');
+//const http = require('http'); // para usar o node puro, sem o express.js
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+// configuração do express.js
+const app = express();
+/* app.use((req, res, next) => {
+    console.log('In the middleware!!');
+    next(); // permite que o request continue para o próximo middleware.
+}); */
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+    console.log('In the another middleware!!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In the another middleware!!');
+    res.send('<h1>Hello from Express.js</h1>'); // o último middleware deve devolver o response usando o método send!
+});
+
+app.listen(3000);
+
+/* const server = http.createServer(app);
+server.listen(3000); */
